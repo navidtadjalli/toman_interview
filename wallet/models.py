7ea07bced660729d4c29f31a6d3be23e6c1876ce
Navidoo <navidta@gmail.com>
2023-06-26 10:20:29 +0330

    lock table on transaction creation

--- wallet/models.py
+++ wallet/models.py
@@ -7,6 +7,39 @@ from django.utils import timezone
 
 from django.utils.translation import gettext_lazy as _
 
+from django.conf import settings
+from django.db import DEFAULT_DB_ALIAS
+from django.db.transaction import Atomic, get_connection
+
+
+class LockedAtomicTransaction(Atomic):
+    """
+    Does a atomic transaction, but also locks the entire table for any transactions, for the duration of this
+    transaction. Although this is the only way to avoid concurrency issues in certain situations, it should be used with
+    caution, since it has impacts on performance, for obvious reasons...
+    """
+
+    def __init__(self, model, using=None, savepoint=None):
+        if using is None:
+            using = DEFAULT_DB_ALIAS
+        super().__init__(using, savepoint, durable=False)
+        self.model = model
+
+    def __enter__(self):
+        super(LockedAtomicTransaction, self).__enter__()
+
+        # Make sure not to lock, when sqlite is used, or you'll run into problems while running tests!!!
+        if settings.DATABASES[self.using]['ENGINE'] != 'django.db.backends.sqlite3':
+            cursor = None
+            try:
+                cursor = get_connection(self.using).cursor()
+                cursor.execute(
+                    'LOCK TABLE {db_table_name}'.format(db_table_name=self.model._meta.db_table)
+                )
+            finally:
+                if cursor and not cursor.closed:
+                    cursor.close()
+
 
 class Wallet(models.Model):
     username = models.CharField(
@@ -29,19 +62,19 @@ class Wallet(models.Model):
     def deposit(self,
                 amount: Decimal,
                 lock_time: int):
-        # TODO lock table to prevent withdrawal
-        self.transactions.create(
-            amount=amount,
-            available_at=timezone.now() + timedelta(seconds=lock_time)
-        )
+        with LockedAtomicTransaction(Transaction):
+            self.transactions.create(
+                amount=amount,
+                available_at=timezone.now() + timedelta(seconds=lock_time)
+            )
 
     def withdraw(self,
                  amount: Decimal):
-        # TODO lock table to prevent racing condition
-        self.transactions.create(
-            amount=amount * -1,
-            available_at=timezone.now()
-        )
+        with LockedAtomicTransaction(Transaction):
+            self.transactions.create(
+                amount=amount * -1,
+                available_at=timezone.now()
+            )
 
     class Meta:
         verbose_name = _("Wallet")
